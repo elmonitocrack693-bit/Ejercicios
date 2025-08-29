1c7804763586b9b888c3027c5b285cfbff0e3532
elmonitocrack693-bit <elmonitocrack693@gmail.com>
2025-08-29 16:43:30 -0500

    Calculadora.py

--- Calculadora.py
+++ Calculadora.py
@@ -8,17 +8,3 @@ def calculadora():
 
     opcion = input("Ingresa el número de la operación (1/2/3/4): ")
 
-    if opcion in ['1', '2', '3', '4']:
-        num1 = float(input("Ingresa el primer número: "))
-        num2 = float(input("Ingresa el segundo número: "))
-
-        if opcion == '1':
-            print("Resultado:", sumar(num1, num2))
-        elif opcion == '2':
-            print("Resultado:", restar(num1, num2))
-        elif opcion == '3':
-            print("Resultado:", multiplicar(num1, num2))
-        elif opcion == '4':
-            print("Resultado:", dividir(num1, num2))
-    else:
-        print("Opción no válida.")
