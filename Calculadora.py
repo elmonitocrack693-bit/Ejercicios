00faf4a048430cc378d20fd17039832e5a515bfb
pibe0710 <ksmz2006@gmail.com>
2025-08-29 17:43:19 -0500

    primera vercion ya unciona y la segunda tambien la suma y a resta

--- Calculadora.py
+++ Calculadora.py
@@ -7,17 +7,18 @@ print("3. Multiplicación")
 print("4. División")
 
 opcion = input("Ingresa el número de la operación (1/2/3/4): ")
+if opcion == "1": 
+   num1 = float(input("ingrese el primer numero"))
+   num2 = float(input("ingrese el segundo numero "))
+   resultado = num1 + num2 
+   print("el resultado de la suma es:",resultado)
 
 if opcion == '2':
     num1 = float (input("ingrese el primer numero"))
     num2 = float (input ("ingrese el segundo numero"))
     resultado = num1-num2
-print(f"{num1} + {num2} = {resultado}")
-if opcion == 1: 
-   num1 = float(input("ingrese el primer numero"))
-   num2 = float(input("ingrese el segundo numero "))
-   resultado = num1 + num2 
-   print("el resultado de la suma es:",resultado)
+    print(f"{num1} + {num2} = {resultado}")
+
    
 
       
